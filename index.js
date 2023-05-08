8c7612fcca7fd970b2daca827254d8217f90dbc0
Simpa Emmanuel <simpaimmey@gmail.com>
2023-05-08 10:46:48 +0100

    connect to mongodb atlas

--- index.js
+++ index.js
@@ -1,27 +1,26 @@
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
-dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-mongoose.connect = (process.env.MONGOLAB_URI,
-{
+const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ij8kr.mongodb.net/contactDB?retryWrites=true&w=majority`;
+
+const connectionParams = {
   useNewUrlParser: true,
-  useFindAndModify: false,
   useUnifiedTopology: true,
-})
+};
+mongoose
+  .connect(url, connectionParams)
   .then(() => {
     console.log("Connected to the database ");
   })
   .catch((err) => {
     console.error(`Error connecting to the database. n${err}`);
   });
-
-// mongoose.connect("mongodb://127.0.0.1:27017/portfilioDB");
 
 const userSchema = new mongoose.Schema({
   name: String,
@@ -41,28 +40,32 @@ const validateEmail = (email) => {
 app.get("/contact", (req, res) => {
   User.find({})
     .then((user) => {
-      res.json(user);
+      res.status(200).json(user);
     })
     .catch((err) => {
-      res.json(err, "omething went wrong");
+      res.status(400).json(err, "omething went wrong");
     });
 });
 
 app.post("/contact", async (req, res) => {
   const { name, email, message } = req.body;
 
-  if (!name || !email || !message) {
-    res.status(404).json("please enter field");
-  } else if (!validateEmail(email)) {
-    res.json("Wrong email format");
-  } else {
-    const data = await User.create([{ name, email, message }]);
-    res.json(data);
+  try {
+    if (!name || !email || !message) {
+      res.status(404).json("please enter field");
+    } else if (!validateEmail(email)) {
+      res.status(400).json("Wrong email format");
+    } else {
+      const data = await User.create({ name, email, message });
+      res.status(200).json(data);
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
   }
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`liistening @ port ${PORT}`);
+  console.log(`listening @ port ${PORT}`);
 });
