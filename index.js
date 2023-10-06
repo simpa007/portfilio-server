93233a8482beb52d54fd665fda1671386ff83ea6
simpa007 <simpaimmey@gmail.com>
2023-10-06 05:19:50 -0700

    changed password

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
@@ -10,62 +9,62 @@ app.use(express.json());
 const url = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.ij8kr.mongodb.net/contactDB?retryWrites=true&w=majority`;
 
 const connectionParams = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
 };
 mongoose
-  .connect(url, connectionParams)
-  .then(() => {
-    console.log("Connected to the database ");
-  })
-  .catch((err) => {
-    console.error(`Error connecting to the database. n${err}`);
-  });
+	.connect(url, connectionParams)
+	.then(() => {
+		console.log("Connected to the database ");
+	})
+	.catch((err) => {
+		console.error(`Error connecting to the database. n${err}`);
+	});
 
 const userSchema = new mongoose.Schema({
-  name: String,
-  email: String,
-  message: String,
-  date: { type: Date, default: Date.now },
+	name: String,
+	email: String,
+	message: String,
+	date: { type: Date, default: Date.now },
 });
 const User = mongoose.model("User", userSchema);
 
 const validateEmail = (email) => {
-  return String(email)
-    .toLowerCase()
-    .match(
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-    );
+	return String(email)
+		.toLowerCase()
+		.match(
+			/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+		);
 };
 app.get("/", (req, res) => {
-  User.find({})
-    .then((user) => {
-      res.status(200).json(user);
-    })
-    .catch((err) => {
-      res.status(400).json(err, "omething went wrong");
-    });
+	User.find({})
+		.then((user) => {
+			res.status(200).json(user);
+		})
+		.catch((err) => {
+			res.status(400).json(err, "something went wrong");
+		});
 });
 
 app.post("/", async (req, res) => {
-  const { name, email, message } = req.body;
+	const { name, email, message } = req.body;
 
-  try {
-    if (!name || !email || !message) {
-      res.status(404).json("please enter field");
-    } else if (!validateEmail(email)) {
-      res.status(400).json("Wrong email format");
-    } else {
-      const data = await User.create({ name, email, message });
-      res.status(200).json(data);
-    }
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+	try {
+		if (!name || !email || !message) {
+			res.status(404).json("please enter field");
+		} else if (!validateEmail(email)) {
+			res.status(400).json("Wrong email format");
+		} else {
+			const data = await User.create({ name, email, message });
+			res.status(200).json(data);
+		}
+	} catch (error) {
+		res.status(400).json({ error: error.message });
+	}
 });
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`listening @ port ${PORT}`);
+	console.log(`listening @ port ${PORT}`);
 });
